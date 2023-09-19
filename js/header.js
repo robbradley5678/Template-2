722f490ff9113cde4286c95489ecce8840379b76
robbradley5678 <robbradley5678@yahoo.com>
2023-09-19 16:32:41 +0100

    Added dropdown menus

--- js/header.js
+++ js/header.js
@@ -12,9 +12,21 @@ function loadContent () {
         let hamburger = document.getElementsByClassName('hamburger')[0];
         let nav = document.querySelector('nav');
         
+        // Show/hide mobile nav
         hamburger.addEventListener('click', function() {
             nav.classList.toggle('move-on-screen');
             hamburger.classList.toggle('hamburger-cross');
+        });
+
+        // Show/hide mobile services dropdown
+        let servicesLink = document.getElementsByClassName('services-link-container')[0];
+        let dropdown = document.getElementsByClassName('services-dropdown')[0];
+        let arrow = document.getElementsByClassName('fa-sort-down')[0];
+
+        servicesLink.addEventListener('click', function() {
+            dropdown.classList.toggle('expand-services-dropdown');
+            arrow.classList.toggle('rotate-arrow');
+            console.log('JS is working so far');
         })
     })
 };
